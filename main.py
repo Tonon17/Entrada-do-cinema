2b7873da6b38f07e7719e93a6d162969e3bff081
Tonon17 <tonondouglas3@gmail.com>
2024-07-13 12:43:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+value = 0
+
+age = int(input("What is your age?"))
+
+if age > 17:
+  value = 18.40 
+  print(f"Your ticket is U${value}")
+else:
+  value = 9.40
+  print(f"Your ticket is U${value}")
